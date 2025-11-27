629c8710dcc562666227f4a6b90703dd24847694
Danny-Caetano <dannykaethanno@gmail.com>
2025-11-27 18:59:57 -0300

    Simulação keylogger

---
+++ Simulação_Keyloger/keylogger.py
@@ -0,0 +1,10 @@
+from pyinput import keyboard
+import time
+import threading
+import logging
+from datetime import datetime
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Configure logging
